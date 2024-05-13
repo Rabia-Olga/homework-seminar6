65df4ff75ec5563c43477838515e3f57baa839d4
Olga <ti-dzhy@yandex.ru>
2024-05-14 02:59:34 +0300

    add tsk1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 1
+//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+string GetstringfromMatrix (char [,] matrix)
+{
+    string str = "";
+    foreach (char e in matrix)
+    str += e;
+    return str;
+}
+
+char [,] chars = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}};
+string result = GetstringfromMatrix(chars);
+Console.WriteLine($"искомая строка из символов массива: {result}");
